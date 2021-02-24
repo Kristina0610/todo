a2ca6b9ccb529ab16216fd55a69a0ea0c38600a8
Kristina0610 <gaevskaya-2011@mail.ru>
2021-02-24 15:40:49 +0300

    Вывод информации о теге на отдельной странице (к какой задаче и какому проекту относится тег)

---
+++ src/routes/tag.route.php
@@ -0,0 +1,42 @@
+<?php 
+$errors = [];
+
+if (isset($_GET['tag_id'])) {
+	$stmt = $pdo->prepare("SELECT * FROM td_tags WHERE id = ?");
+	$stmt->execute([$_GET['tag_id']]);
+	$tag = $stmt->fetchAll();
+
+	if (!$tag) {
+		$errors['tag_not_found'] = "Тег не найден в БД";
+	} else {
+		$stmt = $pdo->prepare("SELECT * FROM td_tasks_tags WHERE tag_id = ?");
+		$stmt->execute([$_GET['tag_id']]);
+		$tasks_id = $stmt->fetchAll();
+
+		if (!$tasks_id) {
+			$errors['tag_dont_have_task'] = "К данному тегу не относится ни одна задача";
+		} else {
+			$items = [];
+			$stmt = $pdo->prepare("SELECT * FROM td_tasks WHERE deleted_at IS NULL AND id IN (SELECT task_id FROM td_tasks_tags WHERE tag_id = ?)");
+			$stmt->execute([$_GET['tag_id']]);
+			$tasks = $stmt->fetchAll();
+			foreach ($tasks as $task) {
+				$stmt_p = $pdo->prepare("SELECT name FROM td_projects WHERE id = ?");
+				$stmt_p->execute([$task['project_id']]);
+				while ($project_name = $stmt_p->fetchColumn()) {
+					$items[] = [
+						"task_id" => $task['id'],
+						"task_name" => $task['name'],
+						"project_id" => $task['project_id'],
+						"project_name" => $project_name
+					];
+				}
+			}
+		}
+	}
+}
+
+$tags = getTagAndCount();
+$projects = getProjects();
+
+include ("../templates/tag.phtml");
